932aafa5bed985c1446a67257a65d56381cf4da7
Prarintita Ratanavarinchai <prarintita@gmail.com>
2019-04-22 21:36:58 -0400

    Added file attachment

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -35,9 +35,28 @@ class MessageForm extends Component {
           autoFocus
           className={css(styles.input)}
         />
+        
+        <input
+          type="file"
+          accept="image/*"
+          capture
+        />
+        {/* <img id="player" controls></img>
+        <script>
+          var recorder = document.getElementById('recorder');
+          player = document.getElementById('player');
+
+          recorder.addEventListener('change', function(e) {
+            var file = e.target.files[0];
+            // Do something with the audio file.
+            player.srcObject = file;
+          });
+        </script> */}
+
         <button type="submit" className={css(styles.button)}>
           <i className="far fa-paper-plane" title="Send"></i>
         </button>
+
       </form>
     )
   }
